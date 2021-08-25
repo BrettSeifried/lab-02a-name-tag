10033a969ba2489acab828253bd8dd637f342126
Brett Seifried <seifriedbrett@gmail.com>
2021-08-25 11:23:55 -0700

    User can now add name

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-bad.code
+// get the elemnt that hodls the nametag name
+const nameElement = document.getElementById('name');
+// get the butoon
+const updateButton = document.getElementById('update-button');
+// the input
+const nameInput = document.getElementById('name-input');
+
+// user clicks the button
+updateButton.addEventListener('click', () => {
+    // get the text in the input
+    const name = nameInput.value;
+    // update the name element with the text
+    nameElement.textContent = name;
+});
